97800d27008b024670e8450992131537aebbca4b
mkordik <mrmeekah@gmail.com>
2023-11-28 18:45:20 -0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/Notion-DoIt-92f232087e654943a7ad94222a2e6613?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
